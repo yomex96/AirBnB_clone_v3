1a56669ed66f8b04b269f7e5af2089cd0c601e2f
yomex96 <abayomirobertonawole2@gmail.com>
2024-04-28 13:57:27 -0700

    work

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,16 @@
 """
 app
 """
-
-from flask import Flask, jsonify
-from flask_cors import CORS
 from os import getenv
+from flask import Flask
+
+#from flask_cors import CORS
+
+from api.v1.views import app_views
+from models import storage
+
+
+app = Flask(__name__)#from flask_cors import CORS
 
 from api.v1.views import app_views
 from models import storage
@@ -13,7 +19,7 @@ from models import storage
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+#CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
@@ -49,4 +55,5 @@ app.register_blueprint(app_views)
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
     PORT = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=HOST, port=PORT, threaded=True
+    app.run(host=HOST, port=PORT, threaded=True)
+                                                
